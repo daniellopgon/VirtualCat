2e65f97c07cfbc732c994fce4e545ea216aa8686
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-22 10:21:45 +0200

    commit 4 delete null in CatDto

--- app/src/main/kotlin/com/example/virtualcatapp/cats/data/remote/dto/CatDto.kt
+++ app/src/main/kotlin/com/example/virtualcatapp/cats/data/remote/dto/CatDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CatDto(
-    val id: Int = null,
+    val id: Int,
     val name: String,
     val hunger: Int,
     val happiness: Int,
