10eb41eabaa8acfc3527f0b4de2f48c2aa0e0cce
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-26 10:16:04 +0200

    commit 6 add mock and junit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,11 +50,13 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("androidx.navigation:navigation-compose:2.7.5")
+
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
