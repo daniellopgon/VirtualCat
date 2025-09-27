fd5d4b677d4bf43cf951150fef1b323be92b20dc
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-09-27 09:53:53 +0200

    commit 8 I fixed the function save in CatDataRepository.kt and CatRepository.kt

--- app/src/main/kotlin/com/example/virtualcatapp/cat/domain/repository/CatRepository.kt
+++ app/src/main/kotlin/com/example/virtualcatapp/cat/domain/repository/CatRepository.kt
@@ -3,5 +3,5 @@ package com.example.virtualcatapp.cat.domain.repository
 import com.example.virtualcatapp.cat.domain.Cat
 
 interface CatRepository {
-    fun saveCat(cat: Cat): Cat
+    fun saveCat(cat: Cat)
 }
---
+++ app/src/main/kotlin/com/example/virtualcatapp/repository/CatDataRepository.kt
@@ -0,0 +1,16 @@
+package com.example.virtualcatapp.cat.repository
+
+import com.example.virtualcatapp.cat.data.local.CatMemLocalDataSource
+import com.example.virtualcatapp.cat.data.local.CatXmlLocalDataSource
+import com.example.virtualcatapp.cat.domain.Cat
+import com.example.virtualcatapp.cat.domain.repository.CatRepository
+
+class CatDataRepository (
+    private val catMemLocalDataSource: CatMemLocalDataSource,
+    private val catXmlLocalDataSource: CatXmlLocalDataSource
+) : CatRepository {
+
+    override fun saveCat(cat: Cat) {
+        catMemLocalDataSource.save(cat)
+    }
+}
